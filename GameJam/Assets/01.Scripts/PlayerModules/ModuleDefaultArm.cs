da6c42443c95f93c1de7cd9d4b0b0ab9b2ceb229
KimM1nSang <minsang1008@naver.com>
2022-08-04 17:18:01 +0900

    디폴트 암 개발

--- GameJam/Assets/01.Scripts/PlayerModules/ModuleDefaultArm.cs
+++ GameJam/Assets/01.Scripts/PlayerModules/ModuleDefaultArm.cs
@@ -9,8 +9,9 @@ public class ModuleDefaultArm : Module
         Vector3 pos = Camera.main.ScreenToWorldPoint( Input.mousePosition);
         pos.z = 0;
         Vector3 playerPos = GameManager.Player.gameObject.transform.position;
-
-        transform.position = playerPos + Vector3.ClampMagnitude((pos - playerPos) , 10);
-
+        Vector3 dir = pos - playerPos;
+        transform.position = playerPos + Vector3.ClampMagnitude(dir , 2.5f);
+        var angle = Mathf.Atan2(pos.y - transform.position.y, pos.x - transform.position.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
     }
 }
