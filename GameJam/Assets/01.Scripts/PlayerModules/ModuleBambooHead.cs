462e5949cbfb9217db5e4e9c4d730aaead0edacd
vndfjsl <28alsrb@gmail.com>
2022-08-04 17:08:53 +0900

    대나무헬리콥터

---
+++ GameJam/Assets/01.Scripts/PlayerModules/ModuleBambooHead.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleBambooHead : Module
+{
+    private float flyTime = 0f;
+
+    public override void ModuleUpdate()
+    {
+        Fly();
+
+        if(GameManager.Player.IsGround)
+        {
+            flyTime = 0f;
+        }
+    }
+
+    private void Fly()
+    {
+        if(GameManager.Player.JumpCount == 2)
+        {
+            if(Input.GetKey(KeyCode.Space) && flyTime <= 1f)
+            {
+                flyTime += Time.deltaTime;
+                float playerDir = Input.GetAxisRaw("Horizontal");
+                GameManager.Player.Rigid.velocity = new Vector2(playerDir * GameManager.Player.PlayerSpeed, 3f);
+            }
+        }
+    }
+}
