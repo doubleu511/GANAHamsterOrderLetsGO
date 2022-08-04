f8972dea29260933d90eee40f1e4ef4fc9186f79
KimM1nSang <minsang1008@naver.com>
2022-08-04 22:49:31 +0900

    폭팔 지뢰

---
+++ GameJam/Assets/01.Scripts/PlayerModules/ModuleMineLuncherArm.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleMineLuncherArm : ModuleDefaultArm
+{
+    public GameObject MinePrefab;
+    private int mineCount = 0;
+
+    private void Start()
+    {
+        Global.Pool.CreatePool<MineObject>(MinePrefab, GameManager.Global.transform);
+    }
+    public override void ArmMoving()
+    {
+        base.ArmMoving();
+        if (Input.GetMouseButtonDown(0) && mineCount < 4)
+        {
+            MineObject mineObj = Global.Pool.GetItem<MineObject>();
+
+            Vector3 playerPos = GameManager.Player.gameObject.transform.position;
+
+            RaycastHit2D hit = Physics2D.Raycast(playerPos, dir.normalized, Vector3.Distance(playerPos, transform.position), LayerMask.GetMask("Ground") + LayerMask.GetMask("Slope"));
+            if (hit.collider != null)
+            {
+                mineObj.transform.position = hit.point;
+                mineObj.isCollision = true;
+            }
+            else
+            {
+                mineObj.transform.position = transform.position;
+            }
+
+            mineObj.MineMove(dir, () => { mineObj.gameObject.SetActive(false); mineCount--; });
+            mineCount++;
+        }
+    }
+}
