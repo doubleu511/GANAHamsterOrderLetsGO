37b5d37d9372dff7368545d1c2465c1ec675d3af
KMJ1324 <eppurdream@gmail.com>
2022-08-04 21:54:49 +0900

    망치 포기하고 갈고리 스크립트 제작

---
+++ GameJam/Assets/01.Scripts/PlayerModules/ModuleHookArm.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleHookArm : ModuleDefaultArm
+{
+
+    public override void ArmMoving()
+    {
+
+    }
+}
