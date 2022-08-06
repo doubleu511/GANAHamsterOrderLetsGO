387f2b0cc7620118fdfc920dc2f1f36e8e4a961e
KMJ1324 <eppurdream@gmail.com>
2022-08-07 08:16:25 +0900

    구글 스프레드 시트 만들어짐

---
+++ GameJam/Assets/01.Scripts/Base/GoogleSheetManager.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class GoogleSheetManager : MonoBehaviour
+{
+	const string URL = "https://script.google.com/macros/s/AKfycbwf54HPVAhcNRzVtCh_3jh2AusLts9DPu9MzgqggZ7oqkscEeXfp_IjWuey9qYv4ORRjA/exec";
+	public InputField NameInput, reviewTxtInput;
+
+    private void Start()
+    {
+		Register("테스트", 10, "ㅁㄴㅇㄹ");
+
+	}
+
+    bool SetIDPass(string name, string reviewTxt)
+	{
+		if (name == "" || reviewTxt == "") return false;
+		else return true;
+	}
+
+
+	public void Register(string name, int timeScore, string reviewTxt)
+	{
+		if (!SetIDPass(name, reviewTxt))
+		{
+			print("아이디 또는 남길말이 비어있습니다");
+			return;
+		}
+
+		WWWForm form = new WWWForm();
+		form.AddField("Name", name);
+		form.AddField("TimeScore", timeScore);
+		form.AddField("ReviewTxt", reviewTxt);
+
+		StartCoroutine(Post(form));
+	}
+
+
+	//public void Login()
+	//{
+	//	if (!SetIDPass())
+	//	{
+	//		print("아이디 또는 비밀번호가 비어있습니다");
+	//		return;
+	//	}
+
+	//	WWWForm form = new WWWForm();
+	//	form.AddField("order", "login");
+	//	form.AddField("id", id);
+	//	form.AddField("pass", pass);
+
+	//	StartCoroutine(Post(form));
+	//}
+
+	IEnumerator Post(WWWForm form)
+	{
+		using (UnityWebRequest www = UnityWebRequest.Post(URL, form)) // 반드시 using을 써야한다
+		{
+			yield return www.SendWebRequest();
+
+			if (www.isDone) /*Response(www.downloadHandler.text);*/
+            {
+				Debug.Log("스프레드에 입력 됨");
+            }
+			else print("웹의 응답이 없습니다.");
+		}
+	}
+
+
+	//void Response(string json)
+	//{
+	//	if (string.IsNullOrEmpty(json)) return;
+
+	//	GD = JsonUtility.FromJson<GoogleData>(json);
+
+	//	if (GD.result == "ERROR")
+	//	{
+	//		print(GD.order + "을 실행할 수 없습니다. 에러 메시지 : " + GD.msg);
+	//		return;
+	//	}
+
+	//	print(GD.order + "을 실행했습니다. 메시지 : " + GD.msg);
+
+	//	if (GD.order == "getValue")
+	//	{
+	//		ValueInput.text = GD.value;
+	//	}
+	//}
+}
