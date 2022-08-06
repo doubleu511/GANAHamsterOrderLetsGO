c734f1b32ad20b2a412b8f6787ce04b77309d53f
KimM1nSang <minsang1008@naver.com>
2022-08-07 03:11:02 +0900

    지뢰 알고리즘 변경

--- GameJam/Assets/01.Scripts/PlayerModules/ModuleMineLauncherArm.cs
+++ GameJam/Assets/01.Scripts/PlayerModules/ModuleMineLauncherArm.cs
@@ -9,41 +9,71 @@ public class ModuleMineLauncherArm : ModuleDefaultArm
     public GameObject MinePrefab;
     public GameObject MineBombVfx;
     private int mineCount = 0;
- 
+    private List<MineObject> mineObjs = new List<MineObject>();
 
     private void Start()
     {
-        Global.Pool.CreatePool<MineObject>(MinePrefab, GameManager.Game.transform);
+        for (int i = 0; i < 2; i++)
+        {
+            MineObject mineObj = Instantiate(MinePrefab, GameManager.Game.transform).GetComponent<MineObject>();
+            mineObj.rigid = mineObj.GetComponent<Rigidbody2D>();
+            mineObj.isLeftMine = i != 0;
+            mineObjs.Add(mineObj);
+        }
+
         Global.Pool.CreatePool<Effect_MineBomb>(MineBombVfx, GameManager.Game.transform,15);
     }
     public override void ArmMoving()
     {
         base.ArmMoving();
+        foreach (var mine in mineObjs)
+        {
+            if (!mine.isLaunched)
+            {
+                mine.transform.position = arms[mine.isLeftMine ? 1 : 0].position;
+                mine.transform.rotation = arms[mine.isLeftMine ? 1 : 0].rotation;
+            }
+        }
+
         if (Input.GetMouseButtonDown(0) && mineCount < 2 && !EventSystem.current.IsPointerOverGameObject())
         {
-            MineObject mineObj = Global.Pool.GetItem<MineObject>();
-            mineObj.rigid = mineObj.GetComponent<Rigidbody2D>();
+            MineObject mineObj = null;
+            foreach (var mine in mineObjs)
+            {
+                if(!mine.isLaunched)
+                {
+                    mineObj = mine;
+                    break;
+                }
+            }
+            if(mineObj != null)
+            {
+                mineObj.isLaunched = true;
 
-            Vector3 anglePos = arms[mineCount].position;
-            //print(mineCount);
-            Debug.DrawRay(anglePos, dirs[mineCount].normalized, Color.red, 1);
-            RaycastHit2D hit = Physics2D.Raycast(anglePos, dirs[mineCount].normalized, .5f, (1 << LayerMask.NameToLayer("Ground")) + (1 << LayerMask.NameToLayer("Slope")));
-            Action OnBombAct = () => { mineCount--; arms[mineCount].GetComponent<SpriteRenderer>().enabled = true; };
-            if (hit.collider != null)
-            {
-                print(hit.collider.name);
-                
-                mineObj.CallOnHit(hit, OnBombAct);
-                StartCoroutine(mineObj.LifeProcess(4, OnBombAct));
+                int armNum = mineObj.isLeftMine ? 1 : 0 ;
+                Transform arm = arms[armNum];
+                arm.GetComponent<SpriteRenderer>().enabled = false;
+                Vector3 anglePos = arm.position;
+                //print(mineCount);
+                Debug.DrawRay(anglePos, dirs[armNum].normalized, Color.red, 1);
+                RaycastHit2D hit = Physics2D.Raycast(anglePos, dirs[armNum].normalized, .5f, (1 << LayerMask.NameToLayer("Ground")) + (1 << LayerMask.NameToLayer("Slope")));
+                Action OnBombAct = () => { mineCount--; arm.GetComponent<SpriteRenderer>().enabled = true; };
+                if (hit.collider != null)
+                {
+                    print(hit.collider.name);
+
+                    mineObj.CallOnHit(hit, OnBombAct);
+                    StartCoroutine(mineObj.LifeProcess(4, OnBombAct));
+                }
+                else
+                {
+                    mineObj.transform.position = arm.position;
+                    mineObj.MineMove(dirs[armNum], OnBombAct);
+                }
+                mineCount++;
+                // 지뢰 런처 발사 효과
             }
-            else
-            {
-                mineObj.transform.position = arms[mineCount].position;
-                mineObj.MineMove(dirs[mineCount], OnBombAct);
-            }
-            arms[mineCount].GetComponent<SpriteRenderer>().enabled = false;
-            mineCount++;
-            // 지뢰 런처 발사 효과
+
         }
     }
 }
